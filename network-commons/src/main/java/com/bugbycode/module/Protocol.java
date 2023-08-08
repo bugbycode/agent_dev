394f3bfd97f0eaf4c1cee4200892c7ca93bdc056
bugby <bugby@LAPTOP-V1OV1CAR>
2023-08-08 15:06:56 +0800

    新增socket4和socket5协议

--- network-commons/src/main/java/com/bugbycode/module/Protocol.java
+++ network-commons/src/main/java/com/bugbycode/module/Protocol.java
@@ -2,9 +2,13 @@ package com.bugbycode.module;
 
 public interface Protocol {
 	
-	public static final int FTP = 1;
+	public static final byte FTP = 1;
 	
-	public static final int HTTP = 0;
+	public static final byte HTTP = 0;
 	
-	public static final int HTTPS = 2;
+	public static final byte HTTPS = 2;
+	
+	public static final byte SOCKET_4 = 0x04;
+	
+	public static final byte SOCKET_5 = 0x05;
 }
