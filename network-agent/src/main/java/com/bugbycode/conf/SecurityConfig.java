dc9028b74c3b6e19568d32a3a41315292485ca3a
Administrator <Administrator@DESKTOP-2FJNJFC>
2024-09-12 20:48:00 +0800

    更新Spring security权限配置

--- network-agent/src/main/java/com/bugbycode/conf/SecurityConfig.java
+++ network-agent/src/main/java/com/bugbycode/conf/SecurityConfig.java
@@ -8,15 +8,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationEventPublisher;
 import org.springframework.security.authentication.DefaultAuthenticationEventPublisher;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Order(0)
 @Configuration
@@ -41,7 +41,25 @@ public class SecurityConfig {
     public DefaultAuthenticationEventPublisher defaultAuthenticationEventPublisher(ApplicationEventPublisher delegate) { 
         return new DefaultAuthenticationEventPublisher(delegate);
     }
-	
+    
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+    	
+    	http.anonymous(any -> any.disable())
+    	
+    	.csrf(csrf -> csrf.disable())
+    	
+    	.authorizeHttpRequests(authorize -> authorize
+    			
+    	        .requestMatchers("/home","/query","/updateForwardById").hasRole("LOGIN")
+    	        
+    	        .anyRequest().authenticated()
+    	        
+    			).formLogin(Customizer.withDefaults());
+    	
+    	return http.build();
+    }
+    
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
