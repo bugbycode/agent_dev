55e9ec58f44e4922ac852a2a8f159d36f0fa40e0
bugbycode <bugbycode@gmail.com>
2025-12-17 15:55:28 +0800

    修改超时参数

--- network-commons/src/main/java/com/bugbycode/config/IdleConfig.java
+++ network-commons/src/main/java/com/bugbycode/config/IdleConfig.java
@@ -1,7 +1,7 @@
 package com.bugbycode.config;
 
 public class IdleConfig {
-	  public static final int READ_IDEL_TIME_OUT = 30; // 读超时
-	  public static final int WRITE_IDEL_TIME_OUT = 30;// 写超时
-	  public static final int ALL_IDEL_TIME_OUT = 120; // 所有超时
+	  public static final int READ_IDEL_TIME_OUT = 10; // 读超时
+	  public static final int WRITE_IDEL_TIME_OUT = 10;// 写超时
+	  public static final int ALL_IDEL_TIME_OUT = 0; // 所有超时
 }
