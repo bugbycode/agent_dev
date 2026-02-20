e77d0907754f2254bd6d1df455425d92cfe17ac7
bugbycode <bugbycode@gmail.com>
2026-02-20 11:46:27 +0800

    修复已知BUG

--- network-commons/src/main/java/com/bugbycode/config/IdleConfig.java
+++ network-commons/src/main/java/com/bugbycode/config/IdleConfig.java
@@ -1,7 +1,7 @@
 package com.bugbycode.config;
 
 public class IdleConfig {
-	  public static final int READ_IDEL_TIME_OUT = 20; // 读超时
-	  public static final int WRITE_IDEL_TIME_OUT = 20;// 写超时
-	  public static final int ALL_IDEL_TIME_OUT = 60; // 所有超时
+	  public static final int READ_IDEL_TIME_OUT = 60; // 读超时
+	  public static final int WRITE_IDEL_TIME_OUT = 60;// 写超时
+	  public static final int ALL_IDEL_TIME_OUT = 20; // 没有数据传输的时间间隔
 }
